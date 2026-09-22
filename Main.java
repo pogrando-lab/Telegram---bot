c21b2a48e44376ff51d21326f94b463e3a9fbba3
pogrando-lab <pogrando@mail.ru>
2026-09-22 22:00:38 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class Main {
-    private static final String BOT_TOKEN = "8937303127:AAFRz6GgLeFLAY1JeJyIcwLPXAhz_NutLcM";
+    private static final String BOT_TOKEN = "8937303127:AAEoR_4seOJNZxvFmmBPzrjPTUawPhBkvUw";
     private static final String TELEGRAM_API = "https://api.telegram.org/bot" + BOT_TOKEN;
 
     public static void main(String[] args) {
